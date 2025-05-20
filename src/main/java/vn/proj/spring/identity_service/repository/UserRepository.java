263698ecf6ed9de355737ac10e3cab8c41597e1e
DucHuy74 <duchuyhl74205@gmail.com>
2025-05-20 21:59:59 +0700

    CRUD API & DB connection

---
+++ src/main/java/vn/proj/spring/identity_service/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package vn.proj.spring.identity_service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import vn.proj.spring.identity_service.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
---
+++ src/main/java/vn/proj/spring/identity_service/service/UserService.java
@@ -0,0 +1,52 @@
+package vn.proj.spring.identity_service.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import vn.proj.spring.identity_service.dto.request.UserCreationRequest;
+import vn.proj.spring.identity_service.dto.request.UserUpdateRequest;
+import vn.proj.spring.identity_service.entity.User;
+import vn.proj.spring.identity_service.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(UserCreationRequest request){
+        User user = new User();
+
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+    }
+
+    public User updateUser(String userId, UserUpdateRequest request) {
+        User user = getUser(userId);
+
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(String userId){
+        userRepository.deleteById(userId);
+    }
+
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+
+    public User getUser(String id){
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+}
